7186b988a769df1150ef55a7605e7b85520f455c
Mircea Bastovoi <mbastovoi@gmail.com>
2019-08-23 17:21:55 +0300

    <commit message>

--- html/wordpress/wp-content/themes/centrul-de-urbanism/header.php
+++ html/wordpress/wp-content/themes/centrul-de-urbanism/header.php
@@ -3,7 +3,7 @@
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta charset="<?php bloginfo('charset');?>">
-  <link href="http://localhost/wordpress/wp-content/themes/centrul-de-urbanism/style.css" rel="stylesheet" type="text/css">
+  <link href="<?php echo get_stylesheet_directory_uri(). '/style.css' ?>" rel="stylesheet" type="text/css">
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
